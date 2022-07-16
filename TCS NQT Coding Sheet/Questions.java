2ebe33e0992c29252b44d307e6eb123319929cf7
Aayush Rana <ayushrana38@gmail.com>
2022-07-16 19:55:46 +0530

    Questions Sheet Added

---
+++ TCS NQT Coding Sheet/Questions.java
@@ -0,0 +1,10 @@
+1. Find the smallest number in an array
+2. Find the largest number in an array
+3. Second Smallest and Second Largest element in an array
+4. Reverse a given array
+5. Count frequency of each element in an array
+6. Rearrange array in increasing-decreasing order
+7. Calculate sum of the elements of the array
+8. Rotate array by K elements - Block Swap Algorithm
+9. Average of all elements in an array
+10. Find the median of the given array
